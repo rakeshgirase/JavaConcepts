6e15538d642ac74db33ceadf2efc246ff8e4b851
appsea <girase.rakesh@gmail.com>
2020-07-10 00:04:47 +0100

    Longest Unique SubString Finder

--- src/main/java/com/exuberant/tutorials/javaconcept/interviews/LongestUniqueSubStringFinder.java
+++ src/main/java/com/exuberant/tutorials/javaconcept/interviews/LongestUniqueSubStringFinder.java
@@ -6,13 +6,13 @@ public class LongestUniqueSubStringFinder {
         String temp = null;
         String max = null;
         for (int i = 0; i < string.length(); i++) {
-            temp = "" + string.charAt(i);
+            temp = String.valueOf(string.charAt(i));
             for (int j = i + 1; j < string.length(); j++) {
                 char character = string.charAt(j);
-                if (temp.contains(String.valueOf(character))) {
-                    break;
-                } else {
+                if (temp.indexOf(character) == -1) {
                     temp = temp + character;
+                } else {
+                    break;
                 }
             }
             if (max == null || max.length() < temp.length()) {
